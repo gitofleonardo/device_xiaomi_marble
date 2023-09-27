08e199d5485bfb6d8c9e5f77cc2a1e3a364064a6
JYRRC <javixhtsweet@gmail.com>
2023-09-27 21:56:12 +0200

    marble: Welcome PixelOS
    Signed-off-by: JYRRC <javixhtsweet@gmail.com>

--- aosp_marble.mk
+++ aosp_marble.mk
@@ -8,8 +8,12 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Inherit from marble device.
 $(call inherit-product, device/xiaomi/marble/device.mk)
@@ -18,7 +22,7 @@ $(call inherit-product, device/xiaomi/marble/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_marble
+PRODUCT_NAME := aosp_marble
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
